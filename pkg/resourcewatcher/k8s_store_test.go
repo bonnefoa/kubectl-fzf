593ed8a9f2cfb6cef723f18d622f405b2c60c28d
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2019-02-09 13:52:57 +0100

    fix test

--- pkg/resourcewatcher/k8s_store_test.go
+++ pkg/resourcewatcher/k8s_store_test.go
@@ -8,7 +8,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/bonnefoa/kubectl-fzf/pkg/k8sresources"
 	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestDumpFullState(t *testing.T) {
@@ -16,15 +18,26 @@ func TestDumpFullState(t *testing.T) {
 	assert.Nil(t, err)
 	defer os.RemoveAll(tempDir)
 
-	k, err := NewK8sStore(func() K8sResource { return &Pod{} }, "pods", tempDir)
+	cfg := watchConfig{
+		resourceName: "pods",
+	}
+	storeConfig := StoreConfig{
+		CacheDir: tempDir,
+	}
+	k, err := NewK8sStore(cfg, storeConfig)
 	assert.Nil(t, err)
 
-	k.data["test"] = &Pod{
-		ResourceMeta: ResourceMeta{name: "Test", creationTime: time.Now()},
-	}
-	k.data["test2"] = &Pod{
-		ResourceMeta: ResourceMeta{name: "Test2", creationTime: time.Now()},
-	}
+	meta1 := metav1.ObjectMeta{Name: "Test",
+		CreationTimestamp: metav1.Time{Time: time.Now()}}
+	meta2 := metav1.ObjectMeta{Name: "Test2",
+		CreationTimestamp: metav1.Time{Time: time.Now()}}
+
+	pod1 := &k8sresources.Pod{}
+	pod1.FromObjectMeta(meta1)
+	pod2 := &k8sresources.Pod{}
+	pod2.FromObjectMeta(meta2)
+	k.data["test"] = pod1
+	k.data["test2"] = pod2
 
 	k.DumpFullState()
 
