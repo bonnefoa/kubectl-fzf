ac73df6bb7ec59f60e7b2b68a4914a0662ce5f8b
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2022-06-27 07:53:35 +0200

    Update of completion logic

--- pkg/completion/completion.go
+++ pkg/completion/completion.go
@@ -3,32 +3,42 @@ package completion
 import (
 	"kubectlfzf/pkg/k8sresources"
 	"kubectlfzf/pkg/util"
+
+	"github.com/sirupsen/logrus"
 )
 
-func getApiCompletion(storeConfig *k8sresources.StoreConfig) []string {
+func getApiCompletion(storeConfig *k8sresources.StoreConfig) ([]string, error) {
 	apiResources := []k8sresources.APIResource{}
 	err := util.LoadFromFile(&apiResources, storeConfig.GetFilePath(k8sresources.ResourceTypeApiResource))
-	util.FatalIf(err)
+	if err != nil {
+		return nil, err
+	}
 	res := []string{}
 	res = append(res, k8sresources.APIResourceHeader)
 	for _, v := range apiResources {
 		res = append(res, v.ToString())
 	}
-	return res
+	return res, nil
 }
 
 // GetResourceCompletion gets the list of the resource specified which begin with `toComplete`.
-func GetResourceCompletion(r k8sresources.ResourceType, storeConfig *k8sresources.StoreConfig) []string {
+func GetResourceCompletion(r k8sresources.ResourceType, namespace *string,
+	storeConfig *k8sresources.StoreConfig) ([]string, error) {
 	if r == k8sresources.ResourceTypeApiResource {
 		return getApiCompletion(storeConfig)
 	}
 	resources := map[string]k8sresources.K8sResource{}
 	err := util.LoadFromFile(&resources, storeConfig.GetFilePath(r))
-	util.FatalIf(err)
+	if err != nil {
+		return nil, err
+	}
 	res := []string{}
 	res = append(res, k8sresources.ResourceToHeader(r))
-	for _, v := range resources {
-		res = append(res, v.ToString())
+	logrus.Debugf("Filterting with namespace %v", namespace)
+	for _, resource := range resources {
+		if namespace == nil || *namespace == resource.GetNamespace() {
+			res = append(res, resource.ToString())
+		}
 	}
-	return res
+	return res, nil
 }
--- pkg/completion/completion_test.go
+++ pkg/completion/completion_test.go
@@ -18,15 +18,33 @@ func getStoreConfig() *k8sresources.StoreConfig {
 
 func TestGetPods(t *testing.T) {
 	storeConfig := getStoreConfig()
-	res := GetResourceCompletion(k8sresources.ResourceTypePod, storeConfig)
+	res, err := GetResourceCompletion(k8sresources.ResourceTypePod, nil, storeConfig)
+	assert.NoError(t, err)
 	t.Log(res)
 	assert := assert.New(t)
 	assert.Contains(res[0], "Cluster")
+	assert.Contains(res[1], "minikube kube-system ")
+	assert.Len(res, 8)
+}
+
+func TestNamespaceFilter(t *testing.T) {
+	storeConfig := getStoreConfig()
+	namespace := "test"
+	res, err := GetResourceCompletion(k8sresources.ResourceTypePod, &namespace, storeConfig)
+	assert.NoError(t, err)
+	t.Log(res)
+	assert := assert.New(t)
+	assert.Len(res, 1)
+
+	namespace = "kube-system"
+	res, err = GetResourceCompletion(k8sresources.ResourceTypePod, &namespace, storeConfig)
+	assert.Len(res, 8)
 }
 
 func TestApiResources(t *testing.T) {
 	storeConfig := getStoreConfig()
-	res := GetResourceCompletion(k8sresources.ResourceTypeApiResource, storeConfig)
+	res, err := GetResourceCompletion(k8sresources.ResourceTypeApiResource, nil, storeConfig)
+	assert.NoError(t, err)
 	t.Log(res)
 	assert := assert.New(t)
 	assert.Contains(res[0], "Fullname")
